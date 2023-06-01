4a42a8ff392bdab541f3063b12bdc04575982677
Mayank Sharma <1611mash@gmail.com>
2023-06-01 19:57:12 +0530

    Added Variables File
    This contains what are variables and how they are used to contain different data types.

---
+++ Variables_py.py
@@ -0,0 +1,23 @@
+# Let's learn varibales
+
+# Variable is a container that holds a specific value.
+# In Python Variables hold a special place as they are used to contain different datatypes
+
+
+x = 123 # x is a variable that holds the value 123
+print(x) # The output would be the value that x contains
+print(type(x))
+
+
+y = 32.57
+print(y)
+print(type(y)) # y holds a float value
+
+
+z = [1,2,3,4,5,6]
+print(z)
+print(type(z)) # z holds a list
+
+t = {1:"x",2:"y",3:"z"}
+print(t)
+print(type(t)) # t holds a dictionary

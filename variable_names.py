0a610fbd6bd82a7ccdfbd3a4fcdc2da688381e4a
Mayank Sharma <1611mash@gmail.com>
2023-06-04 12:06:46 +0530

    new_file generated

---
+++ variable_names.py
@@ -0,0 +1,13 @@
+# How to name variables in Python
+# 1. Variable name should not start with a symbol or digit
+# 2. Variable names are case sensitive
+# 3. Variable names do not contain spaces
+# 4. Variable names can have Underscores
+
+a =123 # Valid Variable name
+123_abc = 345 # Invalid variable name
+MARK = "Name" # Valid
+mark = "name" # Valid
+MARK != mark # True
+my_var_is = "Your Name" # Valid
+@!!!pohg = "My name" # Invalid
